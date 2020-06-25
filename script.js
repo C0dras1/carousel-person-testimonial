1c06420708a7a9dfdd58843e203289b91713877b
Jesse <jessesuderman@gmail.com>
2020-06-24 17:36:59 -0700

    selective hovering if there are more images or not

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ const btnPrev = document.getElementById('btnprev');
 const btnNext = document.getElementById('btnnext');
 const { body } = document;
 
-
-alert('connected');
-
+// Button to go back to the previous image
 btnPrev.addEventListener('click', e => {
   e.preventDefault;
   body.classList.remove('second');
   body.classList.add('first');
 })
 
+// btnPrev.addEventListener('mouseover' e => {
+//   e.preventDefault;
+//   btnPrev.autofocus;
+// })
+
+// Button to go to the next image: adding difference classes to get animation for the transition
 btnNext.addEventListener('click', e => {
   e.preventDefault;
   body.classList.remove('first');
